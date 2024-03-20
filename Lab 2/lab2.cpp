fab5a410569347e151ef007c6d367e52f655b1b6
Dimos <Dimoseek@mail.ru>
2024-03-20 13:05:55 +0200

    Added lab2

---
+++ Lab 2/lab2.cpp
@@ -0,0 +1,17 @@
+#include <array>
+#include <span> // c++ =20
+int LinearSearch(std::span<int> array, int iscomiielement)
+{
+    int tecindex {0};
+
+    
+}
+
+
+
+
+int main()
+{
+  std::array<int, 5> array{0,1,2,3,4};
+  int iscomiielement {3};
+}
