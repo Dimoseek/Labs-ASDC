e3096fd249130c65d1b38505161a416974db1f9c
Dimos <Dimoseek@mail.ru>
2024-03-27 13:05:32 +0200

    SUM

--- Lab 2/lab2.cpp
+++ Lab 2/lab2.cpp
@@ -7,11 +7,36 @@ int LinearSearch(std::span<int> array, int iscomiielement)
     
 }
 
+int sum(int* array0, size_t colElemnt)
+{
+  int* arraysled;
+  int* arrayposl;
+  int  sumelment;
+  arraysled = array0 + 1;
+  while(true)
+  {
+    sumelement = *array0;
+    arrayposl = array0 + colElement - 1;
+    smelement = sumelemnt + *arraysled;
+    if (arraysled == arrayposl)
+    {
+      return sumelement;
+    }
+    arraysled = arraysled + 1;
 
+  }
+  
 
+}
 
 int main()
 {
   std::array<int, 5> array{0,1,2,3,4};
+  std::array<int, 6> array1{0,1,2,3,4,5};
   int iscomiielement {3};
+
+
+  sum(array.data(), array.size());
+  sum(array1.data(), array1.size());
+  
 }
